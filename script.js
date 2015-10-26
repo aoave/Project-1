cca4c45225369af1250fe9d9b254ad006d0cc0b3
Aurelio Avelleyra <Ao@Aurelios-MBP.home>
2015-10-26 16:15:49 -0400

    safe save

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 $(document).ready(function() {
   var app = {
     cards: [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6],
-      init: function() {
-        app.shuffle();
-        app.assignCards();
-      },
-      shuffle: function() {
-        var random = 0
-        var temp = 0
-        for (i = 1; i < app.cards.length; i++) {
-          random = Math.round(Math.random() * i);
-          temp = app.cards[i]
-          app.cards[i] = random;
-          app.cards[random] = temp;
-
-
-        }
-        console.log(app.cards);
+    init: function() {
+      app.shuffle();
+    },
+    shuffle: function() {
+      var random = 0
+      var temp = 0
+      for (i = 1; i < app.cards.length; i++) {
+        random = Math.round(Math.random() * i);
+        temp = app.cards[i]
+        app.cards[i] = random;
+        app.cards[random] = temp;
       }
-      assignCards:
+      app.assignCards();
+      console.log(app.cards);
+    }
+    assignCards: function() {
+      $('.card').each(function(index) {
+        $(this).attr('data-card-value', app.cards[index]);
+      });
+    };
   };
   app.init();
 });
