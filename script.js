608d911fab2b36899a9533978257239e01d2d3a1
Aurelio Avelleyra <Ao@Aurelios-MacBook-Pro.local>
2015-10-26 13:29:11 -0400

    shuffle card tweaks

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-$(document).ready(function(){
+$(document).ready(function() {
   var app = {
-    cards: [1,1,2,2,3,3,4,4,5,5,6,6],
-    init: function() {
-      app.shuffle();
-    }
-    shuffle: function(){
-      var random = 0;
-      var temp = 0;
-      for (i = 1; i < app.cards.length; i++) {
-        random = Math.round(Math.random() * i);
-        temp = app.cards[i];
-        app.cards[i] = app.card[random];
-        app.cards[random] = temp;
+    cards: [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6],
+      init: function() {
+        app.shuffle();
+        app.assignCards();
+      },
+      shuffle: function() {
+        var random = 0
+        var temp = 0
+        for (i = 1; i < app.cards.length; i++) {
+          random = Math.round(Math.random() * i);
+          temp = app.cards[i]
+          app.cards[i] = random;
+          app.cards[random] = temp;
+
+
+        }
+        console.log(app.cards);
       }
-      app.assignCards()
-      console.log("Shuffled: " +app.cards);
-    }
-  }
-})
+      assignCards:
+  };
+  app.init();
+});
